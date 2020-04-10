bbfd019699b7c2d5ddd218b2823231340751b8cd
squarerobin <squarerobin.82@gmail.com>
2020-04-11 01:34:28 +0200

    got rid of window.location.reload(true) and reset state isntead

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "./components/Card/Card"
+import Form from "./components/Form/Form"
 import "./App.scss";
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
           );
         })}
       </div>
+      <Form />
     </div>
   );
 
