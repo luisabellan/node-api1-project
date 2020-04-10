a07ad9f600db5dc9ce6b05bae8ba9e346e14538e
squarerobin <squarerobin.82@gmail.com>
2020-04-11 01:35:24 +0200

    reformatted code

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,56 +2,46 @@ import React, { useState } from "react";
 import axios from "axios";
 import BootstrapForm from "react-bootstrap/Form";
 import BootstrapButton from "react-bootstrap/Button";
-import "./Form.scss"
-
-
+import "./Form.scss";
 
 const usersServer = `http://localhost:5000/users`;
-
 
 function Form(props) {
   const [users, setUsers] = useState([]);
 
-  async function removeUser (e) {
-
-
-      e.preventDefault();
-      await axios
+  async function removeUser(e) {
+    e.preventDefault();
+    await axios
       .delete(`${usersServer}/${props.id}`)
-      
-      .catch(err => `Houston we have an error: ${err}`);
-      
-      
-      
-      let filteredArray = users.filter(u => u !== e.target.value);
-      setUsers(filteredArray);
-      
 
-    
+      .catch((err) => `Houston we have an error: ${err}`);
+
+    let filteredArray = users.filter((u) => u !== e.target.value);
+    setUsers(filteredArray);
   }
 
   return (
     <>
       <BootstrapForm>
-  <BootstrapForm.Group controlId="formBasicEmail">
-    <BootstrapForm.Label>Email address</BootstrapForm.Label>
-    <BootstrapForm.Control type="email" placeholder="Enter email" />
-    <BootstrapForm.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </BootstrapForm.Text>
-  </BootstrapForm.Group>
+        <BootstrapForm.Group controlId="formBasicEmail">
+          <BootstrapForm.Label>Email address</BootstrapForm.Label>
+          <BootstrapForm.Control type="email" placeholder="Enter email" />
+          <BootstrapForm.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </BootstrapForm.Text>
+        </BootstrapForm.Group>
 
-  <BootstrapForm.Group controlId="formBasicPassword">
-    <BootstrapForm.Label>Password</BootstrapForm.Label>
-    <BootstrapForm.Control type="password" placeholder="Password" />
-  </BootstrapForm.Group>
-  <BootstrapForm.Group controlId="formBasicCheckbox">
-    <BootstrapForm.Check type="checkbox" label="Check me out" />
-  </BootstrapForm.Group>
-  <BootstrapButton variant="primary" type="submit">
-    Submit
-  </BootstrapButton>
-</BootstrapForm>
+        <BootstrapForm.Group controlId="formBasicPassword">
+          <BootstrapForm.Label>Password</BootstrapForm.Label>
+          <BootstrapForm.Control type="password" placeholder="Password" />
+        </BootstrapForm.Group>
+        <BootstrapForm.Group controlId="formBasicCheckbox">
+          <BootstrapForm.Check type="checkbox" label="Check me out" />
+        </BootstrapForm.Group>
+        <BootstrapButton variant="primary" type="submit">
+          Submit
+        </BootstrapButton>
+      </BootstrapForm>
     </>
   );
 }
